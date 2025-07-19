9050190b07a4dadd3c4634d1dcaf828e6b032cb6
Dinu Gherman <gherman@darwin.in-berlin.de>
2025-07-19 10:39:08 +0200

    reorg

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const colorButton = document.getElementById('colorButton');
-const body = document.querySelector('body');
-
-const colors = ['#f0f8ff', '#faebd7', '#e6e6fa', '#fff0f5', '#f5f5dc'];
-let currentColorIndex = 0;
-
-colorButton.addEventListener('click', () => {
-    currentColorIndex = (currentColorIndex + 1) % colors.length;
-    body.style.backgroundColor = colors[currentColorIndex];
-});
+console.log("Hello from script.js!");
